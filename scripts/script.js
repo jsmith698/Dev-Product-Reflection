7a0efc787a91d0ff68199153cccebd03ebc32f32
jsmith698 <85500280+jsmith698@users.noreply.github.com>
2021-12-04 23:46:30 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    console.log("Developer Reflection");
+    //var el = document.getElementById("page name");
+    //el.innerText = "Developer Reflection";
+      $("#page name").text("Developer Reflection");
+  });
